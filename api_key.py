d3f66779eb15710c623b8f153641748a858031d2
ElSucho <71868938+ElSucho@users.noreply.github.com>
2024-05-10 21:57:36 +0200

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -1,3 +1,3 @@
 API_KEY = "" # enter API_KEY here
 if API_KEY == "":
-    print(.... gmail.com")
+    print("Please specify your API_KEY in the file api_key.py or contact ladislav.suchy28@gmail.com")
